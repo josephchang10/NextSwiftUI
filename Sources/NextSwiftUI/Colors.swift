c24f924a48244f5b70f07d20b151e2905d079832
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-22 12:44:55 +0800

    Refactor color definitions for macOS compatibility

--- Sources/NextSwiftUI/Colors.swift
+++ Sources/NextSwiftUI/Colors.swift
@@ -9,10 +9,17 @@ import SwiftUI
 
 extension Color {
     struct Next {
-        static let greenCircle = Color(uiColor: .init(named: "greenCircle", in: .module, compatibleWith: nil)!)
-        static let pinkCircle = Color(uiColor: .init(named: "pinkCircle", in: .module, compatibleWith: nil)!)
-        static let backgroundColor = Color(uiColor: .init(named: "backgroundColor", in: .module, compatibleWith: nil)!)
-        static let backgroundColor2 = Color(uiColor: .init(named: "backgroundColor2", in: .module, compatibleWith: nil)!)
+        #if os(macOS)
+        static let greenCircle = Color(nsColor: NSColor(named: "greenCircle", bundle: .module)!)
+        static let pinkCircle = Color(nsColor: NSColor(named: "pinkCircle", bundle: .module)!)
+        static let backgroundColor = Color(nsColor: NSColor(named: "backgroundColor", bundle: .module)!)
+        static let backgroundColor2 = Color(nsColor: NSColor(named: "backgroundColor2", bundle: .module)!)
+        #else
+        static let greenCircle = Color(uiColor: UIColor(named: "greenCircle", in: .module, compatibleWith: nil)!)
+        static let pinkCircle = Color(uiColor: UIColor(named: "pinkCircle", in: .module, compatibleWith: nil)!)
+        static let backgroundColor = Color(uiColor: UIColor(named: "backgroundColor", in: .module, compatibleWith: nil)!)
+        static let backgroundColor2 = Color(uiColor: UIColor(named: "backgroundColor2", in: .module, compatibleWith: nil)!)
+        #endif
     }
     
     static var next: Next.Type { Next.self }
