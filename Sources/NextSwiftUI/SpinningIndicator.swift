69ecd3455e10ad3d6a0040c76daf89c022aadd93
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-22 14:57:19 +0800

    Update SpinningIndicator to be public

--- Sources/NextSwiftUI/SpinningIndicator.swift
+++ Sources/NextSwiftUI/SpinningIndicator.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct SpinningIndicator: View {
+public struct SpinningIndicator: View {
     // MARK: - Properties
     @State private var degree = 270
     @State private var spinnerLength = 0.6
     
-    var body: some View {
+    public var body: some View {
         Circle()
             .trim(from: 0.0, to: spinnerLength)
             .stroke(LinearGradient(colors: [.red, .blue], startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 8.0, lineCap: .round, lineJoin: .round))
