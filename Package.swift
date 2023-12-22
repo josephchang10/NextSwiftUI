50d7024d2ee6f6d52af1c2ec58424428ec57160c
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-22 12:45:59 +0800

    Update macOS platform version to v12 in Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "NextSwiftUI",
     platforms: [
         .iOS(.v15),
-        .macOS(.v10_15)
+        .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
