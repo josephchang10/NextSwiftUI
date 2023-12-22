12bfdc2ad3ac1ffddef7e30bd9cdc38ac29d6d47
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-22 12:39:52 +0800

    Add macOS platform support for version 10.15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "NextSwiftUI",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
