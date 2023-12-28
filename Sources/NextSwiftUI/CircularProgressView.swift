7631642607402651b304c0c0fde05e6147c61bca
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-28 17:14:48 +0800

    "Add CircularProgressView with animation support"

--- Sources/NextSwiftUI/CircularProgressView.swift
+++ Sources/NextSwiftUI/CircularProgressView.swift
@@ -11,7 +11,8 @@ import SwiftUI
 public struct CircularProgressView: View {
     // MARK: - Properties
     @State private var degree = 270
-    @State private var spinnerLength = 0.6
+    @State private var spinnerLength: CGFloat
+    @State private var animate: Bool
     
     public var body: some View {
         Circle()
@@ -22,14 +23,28 @@ public struct CircularProgressView: View {
             .rotationEffect(Angle(degrees: Double(degree)))
             .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false), value: degree)
             .onAppear {
-                degree = 270 + 360
-                spinnerLength = 0
+                if animate {
+                    degree = 270 + 360
+                    spinnerLength = 0
+                }
             }
     }
     
-    public init() {}
+    public init() {
+        animate = true
+        spinnerLength = 0.6
+    }
+    
+    public init<V>(value: V) where V: BinaryFloatingPoint {
+        animate = false
+        spinnerLength = CGFloat(value)
+    }
 }
 
 #Preview {
     CircularProgressView()
+}
+
+#Preview {
+    CircularProgressView(value: Float(70) / 100)
 }
