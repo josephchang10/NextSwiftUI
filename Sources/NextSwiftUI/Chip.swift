56a95ac217b9d099454e1575eb946cf621e4841e
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-28 14:22:30 +0800

    Refactor Chip.swift to use the 'content' variable instead of hardcoded text

--- Sources/NextSwiftUI/Chip.swift
+++ Sources/NextSwiftUI/Chip.swift
@@ -15,7 +15,7 @@ public struct Chip: View {
     let content: AnyView
     
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        content
             .padding()
             .overlay {
                 Capsule()
