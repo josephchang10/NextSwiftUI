bf84ad6a4ca3d182b99fe8d777b71a48f271f008
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-29 16:00:40 +0800

    Add Spinner view to display wait time or process length

--- Sources/NextSwiftUI/Spinner.swift
+++ Sources/NextSwiftUI/Spinner.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+/// Spinner express an unspecified wait time or display the length of a process.
 public struct Spinner: View {
     public var body: some View {
         CircularProgressView(track: Color.clear)
