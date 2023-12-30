1a8d15898a45eb2a41a6e2d47122f84974478b0a
Jiafu Zhang <zhangkuan523@gmail.com>
2023-12-30 17:00:39 +0800

    Add ButtonIcon view and rename Icon.swift to Icons/Icon.swift

---
+++ Sources/NextSwiftUI/Icons/ButtonIcon.swift
@@ -0,0 +1,46 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Jiafu Zhang on 12/30/23.
+//
+
+import SwiftUI
+
+struct ButtonIcon: View {
+    private let content: AnyView
+    
+    var body: some View {
+        content
+            .padding(50 / 4)
+            .background {
+                Circle()
+                    .fill(
+                        LinearGradient(colors: [
+                            .white.opacity(0),
+                            .white.opacity(0.52)
+                        ], startPoint: .top, endPoint: .bottom)
+                        .opacity(0.5)
+                    )
+            }
+            .overlay {
+                Circle()
+                    .stroke(
+                        LinearGradient(colors: [
+                            .white.opacity(0.2),
+                            .white.opacity(0)
+                        ], startPoint: .init(x: 0.25, y: 0), endPoint: .init(x: 0.75, y: 1))
+                    )
+            }
+    }
+    
+    public init(_ content: some View) {
+        self.content = AnyView(content)
+    }
+}
+
+#Preview {
+    ButtonIcon(Image(systemName: "arrow.left.arrow.right"))
+        .backgroundCircles()
+        .preferredColorScheme(.dark)
+}
